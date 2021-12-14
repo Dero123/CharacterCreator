acf0f509b3cc31597468584236b37a1101c314ab
Derek Linden <dlinden2024@chadwickschool.org>
2021-12-14 11:58:27 -0800

    added sound affects and music

--- Character Creator/Assets/Scripts/PlayerController.cs
+++ Character Creator/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
 
     public SpriteRenderer ArmorRenderer;
     public SpriteRenderer WeaponRenderer;
+   
     
 
     public void SetArmor(Sprite newSprite)
